f35fb910307c5535c2ea555f17d2c8aee06594a6
dev2820 <second28200@gmail.com>
2023-09-24 01:14:57 +0900

    chore/ vite alias setup

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vitejs.dev/config/
+const __dirname = path.resolve();
+
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [{ find: "src", replacement: path.resolve(__dirname, "./src") }],
+  },
+});
