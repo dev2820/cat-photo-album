f139dbfce013b84f5f66531fa36f9e2b990c7d11
dev2820 <second28200@gmail.com>
2023-09-24 03:20:16 +0900

    chore/ vue - eslint 설정

---
+++ vue/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: "module"
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  plugins: ["simple-import-sort", "import", "@typescript-eslint"],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+  ],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+  },
+}
