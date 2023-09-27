2b4dffacbf99033c4b9e4214a908c89f6b54e154
dev2820 <second28200@gmail.com>
2023-09-27 17:29:09 +0900

    refactor/ 코드 재정렬

--- vue/src/components/profile/GradientRingAvatar.vue
+++ vue/src/components/profile/GradientRingAvatar.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-import { toRefs } from 'vue';
+  import { toRefs } from 'vue';
 
-import GradientDonut from './GradientDonut.vue';
+  import GradientDonut from './GradientDonut.vue';
 
-interface Props {
-  imgUrl: string;
-  size: number;
-  alt?: string;
-}
+  interface Props {
+    imgUrl: string;
+    size: number;
+    alt?: string;
+  }
 
-const props = withDefaults(defineProps<Props>(), {
-  imgUrl: '',
-  size: 100,
-  alt: '',
-});
+  const props = withDefaults(defineProps<Props>(), {
+    imgUrl: '',
+    size: 100,
+    alt: '',
+  });
 
-const { alt, size, imgUrl } = toRefs(props);
-const strokeWidth = 3;
-const imageSize = size.value - 24;
+  const { alt, size, imgUrl } = toRefs(props);
+  const strokeWidth = 3;
+  const imageSize = size.value - 24;
 </script>
 
 <template>
@@ -27,32 +27,27 @@ const imageSize = size.value - 24;
       :size="size"
       :stroke-width="strokeWidth"
     />
-    <img
-      :src="imgUrl"
-      :width="imageSize"
-      :height="imageSize"
-      :alt="alt"
-    >
+    <img :src="imgUrl" :width="imageSize" :height="imageSize" :alt="alt" />
   </div>
 </template>
 
 <style scoped>
-div {
-  position: relative;
-  width: fit-content;
-  height: fit-content;
-}
-.gradient-circle {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-img {
-  border-radius: 9999px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
+  div {
+    position: relative;
+    width: fit-content;
+    height: fit-content;
+  }
+  .gradient-circle {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  img {
+    border-radius: 9999px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
