2ec79826a638a8eef6ce92befccb4ca9a6c2d962
dev2820 <second28200@gmail.com>
2023-09-27 15:12:52 +0900

    refactor/ vite.config 파일 재정렬

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 const __dirname = path.resolve();
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [{ find: "src", replacement: path.resolve(__dirname, "./src") }],
+    alias: [{ find: 'src', replacement: path.resolve(__dirname, './src') }],
   },
 });
