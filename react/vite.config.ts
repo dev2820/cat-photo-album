60b18f6f0211c3363578029398c84a291716ec5a
dev2820 <second28200@gmail.com>
2023-09-27 17:32:14 +0900

    chore/ alias 셋업

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
-// https://vitejs.dev/config/
+const __dirname = path.resolve();
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: 'src', replacement: path.resolve(__dirname, './src') }],
+  },
 })
