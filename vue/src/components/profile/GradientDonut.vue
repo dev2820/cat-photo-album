e44bea21254e64d4c58a0e9cf30d6e775dd8ae78
dev2820 <second28200@gmail.com>
2023-09-28 19:22:57 +0900

    refactor/ 코드 재정렬

--- vue/src/components/profile/GradientDonut.vue
+++ vue/src/components/profile/GradientDonut.vue
@@ -1,46 +1,30 @@
-<script setup lang='ts'>
-import { toRefs } from "vue";
+<script setup lang="ts">
+  import { toRefs } from 'vue';
 
-interface Props {
-  size: number,
-  strokeWidth: number
-}
-const props = withDefaults(defineProps<Props>(),{
-  size: 100,
-  strokeWidth: 1
-})
+  interface Props {
+    size: number;
+    strokeWidth: number;
+  }
+  const props = withDefaults(defineProps<Props>(), {
+    size: 100,
+    strokeWidth: 1,
+  });
 
-const { size,strokeWidth } = toRefs(props)
+  const { size, strokeWidth } = toRefs(props);
 </script>
 
 <template>
-  <svg
-    :width="size"
-    :height="size"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg :width="size" :height="size" xmlns="http://www.w3.org/2000/svg">
     <defs>
-      <linearGradient
-        id="gradient"
-        x1="0%"
-        y1="0%"
-        x2="100%"
-        y2="0%"
-      >
-        <stop
-          offset="0%"
-          style="stop-color:#f06"
-        />
-        <stop
-          offset="100%"
-          style="stop-color:#9f6"
-        />
+      <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+        <stop offset="0%" style="stop-color: #f06" />
+        <stop offset="100%" style="stop-color: #9f6" />
       </linearGradient>
     </defs>
     <circle
-      :cx="size/2"
-      :cy="size/2"
-      :r="size/2-strokeWidth"
+      :cx="size / 2"
+      :cy="size / 2"
+      :r="size / 2 - strokeWidth"
       stroke="url(#gradient)"
       :stroke-width="strokeWidth"
       fill="none"
@@ -48,5 +32,4 @@ const { size,strokeWidth } = toRefs(props)
   </svg>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
